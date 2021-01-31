fa05369fa9f4155cbcf1acbd06bb2986afb69104
Marcus Emery <marcusemery95@gmail.com>
2021-01-30 23:33:50 -0500

    server deploy

--- server.js
+++ server.js
@@ -1,51 +1,66 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const MOVIES = require('./movies-data-small.json')
+require('dotenv').config();
 
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+const moviesData = require('./movies-data')
+
+const PORT = process.env.PORT || 8080;
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+const app = express();
+
+app.use(morgan(morganSetting));
+app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  next()
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
 })
 
-app.get('/movie', function handleGetMovie(req, res) {
-  let response = MOVIES;
+function equalsIgnoreCase(a, b) {
+    return a.toLowerCase() === b.toLowerCase();
+}
 
-  if (req.query.genre) {
-    response = response.filter(movie =>
-      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
-  }
+function includesIgnoreCase(a, b) {
+    return a.toLowerCase().includes(b.toLowerCase());
+}
 
-  if (req.query.country) {
-    response = response.filter(movie =>
-      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-    )
-  }
+app.get('/movie', (req, res) => {
+    let { genre, country, avg_vote } = req.query;
+    let results = [...moviesData];
 
-  if (req.query.avg_vote) {
-    response = response.filter(movie =>
-      Number(movie.avg_vote) >= Number(req.query.avg_vote)
-    )
-  }
+    if(genre) {
+        results = results.filter((itm) => {
+            return includesIgnoreCase(itm.genre, genre);
+        });
+    }
 
-  res.json(response)
-})
+    if(country) {
+        results = results.filter((itm) => {
+            return equalsIgnoreCase(itm.country, country.replace('+', ' '));
+        });
+    }
 
-const PORT = 9000
+    if(avg_vote) {
+        results = results.filter((itm) => {
+            return itm.avg_vote >= parseFloat(avg_vote);
+        });
+    }
+
+    return res.json(results);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+    console.log("Listening at http://localhost:" + PORT);
+});
